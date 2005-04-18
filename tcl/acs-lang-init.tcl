a49e101320e30b6e9eee7f8994379818f640d6d8
Frank Bergmann <frank.bergmann@project-open.com>
2005-04-18 19:25:53 +0000

    Initial Import

---
+++ tcl/acs-lang-init.tcl
@@ -0,0 +1,10 @@
+ad_library {
+    Do initialization at server startup for the acs-lang package.
+
+    @creation-date 23 October 2000
+    @author Peter Marklund (peter@collaboraid.biz)
+    @cvs-id $Id$
+}
+
+# Cache I18N messages in memory for fast lookups
+lang::message::cache

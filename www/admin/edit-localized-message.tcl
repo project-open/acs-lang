4fdbb393ed26ac41fe3b336834a007e7c6e9712c
Frank Bergmann <frank.bergmann@project-open.com>
2008-06-19 15:58:40 +0000

    - fixed first and last name

--- www/admin/edit-localized-message.tcl
+++ www/admin/edit-localized-message.tcl
@@ -167,12 +167,12 @@ ad_form -extend -name message -form {
 	set package_version [db_string package_version "select max(version_name) from apm_package_versions where package_key = :package_key" -default ""]
 	set system_owner_email [ad_parameter -package_id [ad_acs_kernel_id] SystemOwner "" [ad_system_owner]]
 	set sender_email [db_string sender_email "select email as sender_email from parties where party_id = [ad_get_user_id]" -default $system_owner_email]
-	set sender_firstnames [db_string sender_email "select firstnames from persons where person_id = [ad_get_user_id]" -default "System"]
-	set sender_lastname [db_string sender_email "select lastname from persons where person_id = [ad_get_user_id]" -default "Administrator"]
+	set sender_first_names [db_string sender_email "select first_names from persons where person_id = [ad_get_user_id]" -default "System"]
+	set sender_last_name [db_string sender_email "select last_name from persons where person_id = [ad_get_user_id]" -default "Administrator"]
 	set lang_server_base_url "http://berlin.dnsalias.com/acs-lang/lang-message-register"
 	set lang_server_base_url [parameter::get_from_package_key -package_key "acs-lang" -parameter "LangServerURL" -default $lang_server_base_url]
 	set lang_server_timeout [parameter::get_from_package_key -package_key "acs-lang" -parameter "LangServerTimeout" -default 5]
-	set lang_server_url [export_vars -base $lang_server_base_url {locale package_key message_key message comment package_version sender_email sender_firstnames sender_lastname}]
+	set lang_server_url [export_vars -base $lang_server_base_url {locale package_key message_key message comment package_version sender_email sender_first_names sender_last_name}]
 	
 	ns_httpget $lang_server_url $lang_server_timeout
     } err_msg

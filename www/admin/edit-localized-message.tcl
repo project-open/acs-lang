68cff75292355bf6316f109045cd2e7f5b1a4512
Frank Bergmann <frank.bergmann@project-open.com>
2008-06-20 14:16:43 +0000

    - fixed conflict

--- www/admin/edit-localized-message.tcl
+++ www/admin/edit-localized-message.tcl
@@ -15,6 +15,7 @@ ad_page_contract {
     show:optional
     {usage_p "f"}
     {return_url {}}
+    {submit_remote_p "1" }
 }
 
 # We rename to avoid conflict in queries
@@ -65,6 +66,8 @@ if { ![string equal $default_locale $current_locale] } {
         }
     }
 }
+
+set submit_remote_options_list [list [list "Submit to translation server" 1]]
     
 ad_form -extend -name message -form {
     {message:text(textarea)
@@ -74,6 +77,10 @@ ad_form -extend -name message -form {
     {comment:text(textarea),optional
         {label "Comment"}
         {html { rows 6 cols 40 }}
+    }
+    {submit_remote_p:text(checkbox),optional
+	{label "" } 
+	{options $submit_remote_options_list} 
     }
     {submit:text(submit)
         {label "     Update     "}
@@ -161,21 +168,10 @@ ad_form -extend -name message -form {
     # Register message via acs-lang
     lang::message::register -comment $comment $locale $package_key $message_key $message
 
-
-    # Send a message to the language server
-    catch {
-	set package_version [db_string package_version "select max(version_name) from apm_package_versions where package_key = :package_key" -default ""]
-	set system_owner_email [ad_parameter -package_id [ad_acs_kernel_id] SystemOwner "" [ad_system_owner]]
-	set sender_email [db_string sender_email "select email as sender_email from parties where party_id = [ad_get_user_id]" -default $system_owner_email]
-	set sender_first_names [db_string sender_email "select first_names from persons where person_id = [ad_get_user_id]" -default "System"]
-	set sender_last_name [db_string sender_email "select last_name from persons where person_id = [ad_get_user_id]" -default "Administrator"]
-	set lang_server_base_url "http://berlin.dnsalias.com/acs-lang-server/lang-message-register"
-	set lang_server_base_url [parameter::get_from_package_key -package_key "acs-lang" -parameter "LangServerURL" -default $lang_server_base_url]
-	set lang_server_timeout [parameter::get_from_package_key -package_key "acs-lang" -parameter "LangServerTimeout" -default 5]
-	set lang_server_url [export_vars -base $lang_server_base_url {locale package_key message_key message comment package_version sender_email sender_first_names sender_last_name}]
-	
-	ns_httpget $lang_server_url $lang_server_timeout
-    } err_msg
+    # Register on remote server
+    if {1 == $submit_remote_p} {
+	lang::message::register_remote -comment $comment $locale $package_key $message_key $message
+    }
 
     if { [empty_string_p $return_url] } {
         set return_url "[ad_conn url]?[export_vars { locale package_key message_key show }]"
